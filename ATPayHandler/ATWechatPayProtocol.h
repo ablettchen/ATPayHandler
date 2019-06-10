386e53a77c6604b1af8e92f489c43e05fef2ae79
ablett <ablett.chen@gmail.com>
2019-06-10 10:46:47 +0800

    mod

--- ATPayHandler/ATWechatPayProtocol.h
+++ ATPayHandler/ATWechatPayProtocol.h
@@ -10,22 +10,13 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @protocol ATWechatPayProtocol <NSObject>
-
-/** 平台标识 */
-@property (copy, nonatomic) NSString *openID;
-/** 商家向财付通申请的商家id */
-@property (nonatomic, retain) NSString *partnerId;
-/** 预支付订单 */
-@property (nonatomic, retain) NSString *prepayId;
-/** 随机串，防重发 */
-@property (nonatomic, retain) NSString *nonceStr;
-/** 时间戳，防重发 */
-@property (nonatomic, assign) UInt32 timeStamp;
-/** 商家根据财付通文档填写的数据和签名 */
-@property (nonatomic, retain) NSString *package;
-/** 商家根据微信开放平台文档对数据做的签名 */
-@property (nonatomic, retain) NSString *sign;
-
+@property (copy, nonatomic) NSString *openID;       ///< 平台标识
+@property (nonatomic, copy) NSString *partnerId;    ///< 商家向财付通申请的商家id
+@property (nonatomic, copy) NSString *prepayId;     ///< 预支付订单
+@property (nonatomic, copy) NSString *nonceStr;     ///< 随机串，防重发
+@property (nonatomic, assign) UInt32 timeStamp;     ///< 时间戳，防重发
+@property (nonatomic, copy) NSString *package;      ///< 商家根据财付通文档填写的数据和签名
+@property (nonatomic, copy) NSString *sign;         ///< 商家根据微信开放平台文档对数据做的签名
 @end
 
 NS_ASSUME_NONNULL_END
